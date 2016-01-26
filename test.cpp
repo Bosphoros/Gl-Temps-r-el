27ed52f4f6b039caaff2e742cfccc233ea3777f9
Bosphoros <floriantd.adam@gmail.com>
2016-01-26 16:01:12 +0100

    Vertices indexed

--- test.cpp
+++ test.cpp
@@ -36,6 +36,8 @@ glm::mat4 lookat = glm::lookAt(camPos, glm::vec3(0,0,0), glm::vec3(0,0,1));
 glm::mat4 perspective = glm::perspective(glm::radians(60.0f), float(4)/float(3), 0.1f, 100.0f);
 
 std::vector<point3> points;
+std::vector<unsigned int> indices;
+glm::vec3 light(0, 1, 0); 
 
 /*glm::vec4 p3(0.5,0.5,0,1);
 glm::vec4 p4(-0.5,0.5,0,1);*/
@@ -210,9 +212,13 @@ struct
 } gs;
 
 void fillPoints(){
+	for(int i = 0; i < mesh.points.size(); ++i) {
+		points.push_back(mesh.points.at(i));
+		//points.push_back(mesh.normalesPoints.at(i));
+	}
+
 	for(int i = 0; i < mesh.faces.size(); ++i) {
-		
-		points.push_back(mesh.points.at(mesh.faces.at(i)));
+		indices.push_back(mesh.faces.at(i));
 	}
 }
 
@@ -222,6 +228,8 @@ void init()
 	mesh = reader.import("bunny.off");
 	mesh.center();
 	mesh.normalize();
+	mesh.norms();
+	mesh.normsPoints();
 	
 	fillPoints();
 
@@ -230,25 +238,23 @@ void init()
 	glGenBuffers(1, &(gs.buffer));
 	glBindBuffer(GL_ARRAY_BUFFER, gs.buffer);
 	//glBufferData(GL_ARRAY_BUFFER, sizeof(double)*3*points.size(), &points[0], GL_STATIC_DRAW);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(double)*3*mesh.points.size(), &(mesh.points)[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*3*points.size(), &points[0], GL_STATIC_DRAW);
 
 
 	glCreateVertexArrays(1, &gs.vao);
 	glBindVertexArray(gs.vao);
 
 	glBindBuffer(GL_ARRAY_BUFFER, gs.buffer);
-	glVertexAttribPointer(10, 3, GL_DOUBLE, GL_FALSE, sizeof(double)*3, 0);
+	glVertexAttribPointer(10, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3, 0);
 	glEnableVertexAttribArray(10);
+
+	/*glBindBuffer(GL_ARRAY_BUFFER, gs.buffer);
+	glVertexAttribPointer(11, 3, GL_FLOAT, GL_FALSE, sizeof(float)*2*3, (void *) (sizeof(float)*3));
+	glEnableVertexAttribArray(11);*/
 
 	glGenBuffers(1, &(gs.vbo));
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gs.vbo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.faces.size() * sizeof(int), &(mesh.faces)[0], GL_STATIC_DRAW);
-	
-	for(int i = 0 ; i < mesh.faces.size(); ++i) {
-		if(!(points.at(i) == mesh.points.at(mesh.faces.at(i)))) {
-			std::cout << "Bouh" << std::endl;
-		}
-	}
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 
 	glBindVertexArray(0);
 }
@@ -262,11 +268,11 @@ void render(GLFWwindow* window)
 	glClear(GL_COLOR_BUFFER_BIT);
 	glUseProgram(gs.program);
 	glBindVertexArray(gs.vao);
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gs.vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gs.vbo);
 
 	{
 		//glDrawArrays(GL_TRIANGLES, 0, points.size());
-		glDrawElements(GL_TRIANGLES, mesh.faces.size(), GL_INT, (void*)0);
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, (void*)0);
 	}
 
 	glBindVertexArray(0);
@@ -280,6 +286,7 @@ void render(GLFWwindow* window)
 
 	glProgramUniform1f(gs.program, 4, v);
 	glProgramUniformMatrix4fv(gs.program, 5, 1, false, &mat[0][0]);
+	glProgramUniform3fv(gs.program, 6, 1, &light[0]);
 	/*glProgramUniform4fv(gs.program, 0, 1, &p1centre[0]);
 	glProgramUniform4fv(gs.program, 1, 1, &pextend[0]);*/
 
